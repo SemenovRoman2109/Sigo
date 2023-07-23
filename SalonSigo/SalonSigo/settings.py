1dee5a5eceb18c0be168bc5ae0506c0bb9a5df5d
Roman <romansemenov2109@gmail.com>
2023-07-23 19:12:01 +0300

    fix-settings

--- SalonSigo/SalonSigo/settings.py
+++ SalonSigo/SalonSigo/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=sacjcn^w!13(vvn(*l90_ushvghglsnhg9yedro)4yy#*ynf9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sigosalon.pythonanywhere.com"]
 
 
 # Application definition
